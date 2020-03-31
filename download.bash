a31a83247dc0a6fefed9948e5a80cc4ffc442c4d
Simon Niklaus <simon.niklaus@outlook.com>
2020-03-31 09:55:17 -0700

    no message

--- download.bash
+++ download.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget --continue --timestamping --output-document=models/disparity-estimation.pytorch http://content.sniklaus.com/kenburns/network-disparity.pytorch
-wget --continue --timestamping --output-document=models/disparity-refinement.pytorch http://content.sniklaus.com/kenburns/network-refinement.pytorch
-wget --continue --timestamping --output-document=models/pointcloud-inpainting.pytorch http://content.sniklaus.com/kenburns/network-inpainting.pytorch
+wget --verbose --continue --timestamping --output-document=models/disparity-estimation.pytorch http://content.sniklaus.com/kenburns/network-disparity.pytorch
+wget --verbose --continue --timestamping --output-document=models/disparity-refinement.pytorch http://content.sniklaus.com/kenburns/network-refinement.pytorch
+wget --verbose --continue --timestamping --output-document=models/pointcloud-inpainting.pytorch http://content.sniklaus.com/kenburns/network-inpainting.pytorch
